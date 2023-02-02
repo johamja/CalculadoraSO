755180ebbc9c293fcd1f70b982fa39e20c0b55c3
johamja <johamfranco318@gmail.com>
2023-02-02 13:55:35 -0500

    Operaciones matematicas

---
+++ Calculadora/OperacionesMatematicas.c
@@ -0,0 +1,89 @@
+#include <math.h>
+
+// function definition
+
+/* 
+    @param a primer elemento
+    @param b segundo elemento
+    @return a+b
+*/
+int Suma(int a, int b)
+{
+    return a + b;
+}
+
+/* 
+    @param a primer elemento
+    @param b segundo elemento
+    @return a-b
+*/
+int Resta(int a, int b)
+{
+    return a - b;
+}
+
+/* 
+    @param a primer elemento
+    @param b segundo elemento
+    @return a*b
+*/
+int Multiplicacion(int a, int b)
+{
+    return a * b;
+}
+
+/* 
+    @param a primer elemento
+    @param b segundo elemento
+    @return a/b
+*/
+int Division (int a, int b)
+{
+    return a / b;
+}
+
+/* 
+    @param a primer elemento
+    @return raiz cuadrada de a
+*/
+int RaizCuadrada(int a)
+{
+    return sqrt(a);
+}
+
+/* 
+    @param a primer elemento
+    @return sen(a)
+*/
+int Seno(int a)
+{
+    return sin(a);
+}
+
+/* 
+    @param a primer elemento
+    @return cos(a)
+*/
+int Coseno(int a)
+{
+    return cos(a);
+}
+
+/* 
+    @param a primer elemento
+    @return tan(a)
+*/
+int Tangente(int a)
+{
+    return tan(a);
+}
+
+/* 
+    @param a primer elemento
+    @param b segundo elemento
+    @return a^b
+*/
+int Potencia(int a, int b)
+{
+    return pow(a,b);
+}
