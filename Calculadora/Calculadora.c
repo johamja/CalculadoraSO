4925a6d6484cb861388fbe5edab57cf7849eea00
johamja <johamfranco318@gmail.com>
2023-02-02 14:48:09 -0500

    operaciones matematicas se pueden usar en otros archivos

---
+++ Calculadora/Calculadora.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+#include "OpeMate.h"
+
+
+int main()
+{
+    return 0;
+}
---
+++ Calculadora/OpeMate.h
@@ -0,0 +1,11 @@
+// Todas las operacion en el archvio OpeMate.c
+
+int Suma(int a, int b);
+int Resta(int a, int b);
+int Multiplicacion(int a, int b);
+int Division(int a, int b);
+int RaizCuadrada(int a);
+int Seno(int a);
+int Coseno(int a);
+int Tangente(int a);
+int Potencia(int a, int b);
